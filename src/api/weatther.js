87c6b4f5e904008eacb931ca1e25e278ed24f611
MuhiaKevin <archerkreiger@gmail.com>
2020-08-21 16:09:13 +0300

    initial commit

---
+++ src/api/weatther.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const axios = require('axios');
+
+const router = express.Router();
+
+
+const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?`
+
+router.get('/', async (req, res, next) => {
+    const { city } = req.query;
+    // make a request to weather api
+    try {
+        const params = {
+            units: "metric",
+            api_key: process.env.OPENWEATHER_APIKEY
+        }
+
+        params.q = city === undefined ? "naivasha,kenya" : city
+        const { data } = await axios.get(`${BASE_URL}q=${params.q}&units=${params.units}&APPID=${params.api_key}`)
+
+        // respond with data from openweather api
+        res.json(data)
+
+    } catch (error) {
+        next(error)
+    }
+});
+
+module.exports = router;
